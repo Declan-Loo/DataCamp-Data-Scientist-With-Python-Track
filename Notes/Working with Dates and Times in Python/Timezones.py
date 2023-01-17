c66c126a426486bc1b911d5e47edafca7e31ca35
Declan Loo <188declan@gmail.com>
2023-01-17 17:54:00 +0800

    Update Timezones.py

--- Notes/Working with Dates and Times in Python/Timezones.py
+++ Notes/Working with Dates and Times in Python/Timezones.py
@@ -1,3 +1,20 @@
+# Import datetime, timedelta, timezone
+from datetime import datetime, timedelta, timezone
+
+# Create a timezone for Pacific Standard Time, or UTC-8
+pst = timezone(timedelta(hours=-8))
+
+# October 1, 2017 at 15:26:26, UTC-8
+dt = datetime(2017, 10, 1, 15, 26, 26, tzinfo=pst)
+
+# Print results
+print(dt.isoformat())
+
+print(dt.astimezone(timezone.utc)) #Print the datetime in UTC
+
+# Create a timezone object corresponding to UTC-4
+edt = timezone(timedelta(hours = -4))
+
 # Import datetime
 from datetime import datetime
 
