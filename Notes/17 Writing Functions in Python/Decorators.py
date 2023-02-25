43f70fa8e490f490e05a622fbe86b13389408f11
Declan Loo <188declan@gmail.com>
2023-02-26 01:08:13 +0800

    Update Decorators.py

--- Notes/17 Writing Functions in Python/Decorators.py
+++ Notes/17 Writing Functions in Python/Decorators.py
@@ -234,3 +234,23 @@ def hello_goodbye(name):
   return '\n{}\n{}\n'.format(hello(name), goodbye(name))
   
 print(hello_goodbye('Alice'))
+
+#Tag your functions
+def tag(*tags):
+  # Define a new decorator, named "decorator", to return
+  def decorator(func):
+    # Ensure the decorated function keeps its metadata
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+      # Call the function being decorated and return the result
+      return func(*args, **kwargs)
+    wrapper.tags = tags
+    return wrapper
+  # Return the new decorator
+  return decorator
+
+@tag('test', 'this is a tag')
+def foo():
+  pass
+
+print(foo.tags)
